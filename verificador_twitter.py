52861415b177270227bc25330c8d06ccc661024c
felipemedola <felipe.medola@hotmail.com>
2024-05-15 09:58:35 -0300

    Add files via upload
    Meu objetivo neste projeto foi criar um programa simples que determinasse se um texto específico poderia ser postado como um tuíte no Twitter, respeitando o limite de 140 caracteres. Ao receber uma linha de texto como entrada, meu objetivo era desenvolver uma solução que indicasse se o texto poderia ser publicado como um tuíte ou se excederia o limite de caracteres.
    
    Compreender e resolver esse desafio não apenas me permitiu aplicar meus conhecimentos em programação, mas também me desafiou a encontrar uma solução eficiente para um problema da realidade. Ao longo deste projeto, visei criar um programa simples, porém funcional, que pudesse ser útil para usuários do Twitter ao determinar se seus textos atendiam aos requisitos de tamanho para publicação na plataforma.

---
+++ verificador_twitter.py
@@ -0,0 +1,12 @@
+LIMITE_TWEET = 140
+
+T = input("No que está pensando hoje?\nTweet: ")
+
+quantidade_caracteres = len(T)
+
+if quantidade_caracteres <= LIMITE_TWEET:
+    print("Seu texto cabe perfeitamente em um tuíte!")
+    print("* TWEET *")
+else:
+    print("Oops! Seu texto ultrapassou o limite de 140 caracteres.")
+    print("* MUTE *")
